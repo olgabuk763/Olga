a852dc5635224642690912f74bfd48c5eb4e39c0
olgabuk763 <olgabuk763@gmail.com>
2023-12-21 14:32:47 +0300

    Доработкк фпйла start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-gcc -o start MyProject091123.c 1>log.txt 2>err.txt
+name=Test
+password=Test1234
 
-if [ -s err.txt ]
-then
-	echo "Ошибка компиляции"
-	cat err.txt
+echo "Доступ к программе"
+echo "Введите имя пользователя:"
+read u_name
+echo "Введите пароль:"
+read u_pass
+
+if [[ "$u_name" != "$name" ]] || [[ "$u_pass" != "$password" ]];
+	then
+	echo "Ошибка логина или пароля"
 else
-	echo "Старт программы"
-	echo "Введите два числа a и b"
-	./start > res.txt
-	cat res.txt
+	gcc -o start MyProject091123.c 1>log.txt 2>err.txt
+	if [ -s err.txt ]
+		then
+			echo "Ошибка компиляции"
+			cat err.txt
+		else
+			echo "Старт программы"
+			echo "Введите два числа a и b"
+			./start > res.txt
+			cat res.txt
+		fi
 fi
